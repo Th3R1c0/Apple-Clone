e7e440e2a681751fe81b4d41a9c2ed85f47f651c
Misty <RicoTS114@protonmail.com>
2023-01-28 11:24:31 +0000

    backend update, integration with sanity

---
+++ apple-clone/schemas/index.ts
@@ -0,0 +1,14 @@
+export const schemaTypes = [
+    {
+        title: "My Example Document Type",
+        name: "exampleDocumentType",
+        type: "document",
+        fields: [
+          {
+            title: "Greeting",
+            name: "greeting",
+            type: "string"
+          }
+        ]
+      } 
+]
